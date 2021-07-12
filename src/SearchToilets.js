e2331e44cfe64a84671d1fc23853feb419b10465
Tina Iuso <tinaiuso@gmail.com>
2021-07-12 18:23:42 +0100

    useState to fix using real data

--- src/SearchToilets.js
+++ src/SearchToilets.js
@@ -1,31 +1,30 @@
 import React from 'react';
 export default function SearchToilets() {
+  //states -input query, toilets, hooks
+  const [query, setQuery] = useState('');
+  //create the state for toilets and update that state when appropriate
+  const [toilets, setToilets] = useState('');
 
   const searchToilets = async(e) => {
     e.preventDefault();
-    console.log("submitting");
     const query = "TW2";
 
     const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
 
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data);
 
     try {
       const res = await fetch(url);
       const data = await res.json();
-      console.log(data);
+      setToilets(data.results);
     }catch(err){
       console.error(err);
     }
-
   }
   return (
     <form className="form" onSubmit={searchToilets}>
       <label className="label" htmlFor="query">Postcode</label>
       <input className="input" type="text" name="query"
-      placeholder="i.e. TW2" />
+      placeholder="i.e. TW2" value={query} onChange={(e) => setQuery(e.target.value)} />
       <button className="button" type="submit">Search</button>
     </form>
   )
